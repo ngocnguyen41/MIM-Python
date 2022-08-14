3d81d1689ac946ec44d0cd6711cd7530c90d298a
ngocnguyen41 <109148839+ngocnguyen41@users.noreply.github.com>
2022-08-14 19:04:35 +0700

    Add files via upload

---
+++ week03/week03_student38_NguyenVietNgoc/week03_assignment02_student38_NguyenVietNgoc.py
@@ -0,0 +1,8 @@
+bigNumbers = 'bigNumber.txt'
+with open(bigNumbers, 'r') as myFile:
+    data = myFile.read().splitlines()
+data = [int(x) for x in data]
+sumBigNumbers = sum(data)
+print ("The first ten digits of the sum of the following one hundred 50 digit numbers: " 
+        + str(sumBigNumbers)[:10])  # 5537376230
+
